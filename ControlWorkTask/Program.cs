983e56f530e4f4a0748d9e7685351e18fd519fc3
Vyacheslav <semionoff.slav@yandex.ru>
2022-10-02 18:47:51 +0300

    Добавил решение контрольной работы

---
+++ ControlWorkTask/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+string[] massive1 = new string[5] {"123", "56", "hello", "world", "num"};
+DecisionTask(massive1);
+
+void DecisionTask(string[] massive1)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < massive1.Length; i++)
+    {
+        if(massive1[i].Length <= 3)
+        {
+            Console.Write($"{massive1[i]} ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
